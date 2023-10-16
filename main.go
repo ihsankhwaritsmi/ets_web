5a8fd07199c0837487d0924b22fb70854dc0fa2b
ihsankhwaritsmi <alkhwaritsmi@gmail.com>
2023-10-16 11:29:05 +0700

    modified:   index.html 	modified:   main.go 	deleted:    style.css

--- main.go
+++ main.go
@@ -9,8 +9,10 @@ import (
 )
 
 type Currency struct {
-	Rate   string `json:"rate"`
-	Status string `json:"status"`
+	Rates map[string]struct {
+		Rate   float64 `json:"rate"`
+		Status float64 `json:"status"`
+	} `json:"rates"`
 }
 
 type AllCurrency struct {
@@ -19,7 +21,7 @@ type AllCurrency struct {
 
 func main() {
 
-	apiUrl := "https://www.freeforexapi.com/api/live"
+	apiUrl := "https://www.freeforexapi.com/api/live?pairs=EURUSD,USDJPY,NZDUSD"
 
 	response, err := http.Get(apiUrl)
 	if err != nil {
@@ -29,36 +31,61 @@ func main() {
 
 	defer response.Body.Close()
 
-	var curr AllCurrency
-	// var curren AllCurrency
+	// var curr Rates
+	var curr Currency
 
 	err = json.NewDecoder(response.Body).Decode(&curr)
 	if err != nil {
 		fmt.Println("Error decoding JSON", err)
 		return
 	}
-	fmt.Println(curr.Results)
 
-	f1 := func(w http.ResponseWriter, r *http.Request) {
+	// fmt.Println(curr.Rates["EURUSD"].Rate)
+	fmt.Printf("EUR/USD Rate: %.2f\n", curr.Rates["EURUSD"].Rate)
+	fmt.Printf("EUR/USD Rate: %.2f\n", curr.Rates["USDJPY"].Rate)
+	fmt.Printf("EUR/USD Rate: %.2f\n", curr.Rates["NZDUSD"].Rate)
+	// fmt.Println(curren.Results)
+
+	f := func(w http.ResponseWriter, r *http.Request) {
 		tmpl := template.Must(template.ParseFiles("index.html"))
 		tmpl.Execute(w, nil)
 	}
 
-	f2 := func(w http.ResponseWriter, r *http.Request) {
-		for _, str := range curr.Results {
-			// fmt.Println(str)
-			htm := str
-			htmlStr := fmt.Sprintf(`<div class="card m-sm-3 border-danger m-4" style="width: 18rem;">
-            <div class="card-body" >
-                <h4 class="card-body">%s</h2>
-                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-            </div></div>`, htm)
-			tmpl, _ := template.New("t").Parse(htmlStr)
-			tmpl.Execute(w, nil)
-		}
+	f1 := func(w http.ResponseWriter, r *http.Request) {
+		htmlStr := fmt.Sprintf(`
+					<h4 class="card-body text-info">EURUSD</h2>
+					<p class="card-text">%f</p>
+				`, curr.Rates["EURUSD"].Rate)
+
+		tmpl, _ := template.New("t").Parse(htmlStr)
+		tmpl.Execute(w, nil)
 	}
-	http.HandleFunc("/", f1)
-	http.HandleFunc("action", f2)
-	log.Fatal(http.ListenAndServe(":7000", nil))
+	f2 := func(w http.ResponseWriter, r *http.Request) {
+		htmlStr := fmt.Sprintf(`
+					<h4 class="card-body text-info">USDJPY</h2>
+					<p class="card-text">%f</p>
+				`, curr.Rates["USDJPY"].Rate)
+
+		tmpl, _ := template.New("t").Parse(htmlStr)
+		tmpl.Execute(w, nil)
+	}
+	f3 := func(w http.ResponseWriter, r *http.Request) {
+		htmlStr := fmt.Sprintf(`
+					<h4 class="card-body text-info">NZDUSD</h2>
+					<p class="card-text">%f</p>
+				`, curr.Rates["NZDUSD"].Rate)
+
+		tmpl, _ := template.New("t").Parse(htmlStr)
+		tmpl.Execute(w, nil)
+	}
+
+	http.HandleFunc("/", f)
+	http.HandleFunc("/action1", f1)
+	http.HandleFunc("/action2", f2)
+	http.HandleFunc("/action3", f3)
+
+	fs := http.FileServer(http.Dir("style.css"))
+	http.Handle("/css/", http.StripPrefix("/css", fs))
+	log.Fatal(http.ListenAndServe(":8000", nil))
 
 }
